f9d352c0bd7ce3c8ce5b11fa301ca2c7ef7d7813
James Beams <jamesmbeams@gmail.com>
2024-03-08 14:25:40 -0600

    Update lint command in package.json to include typescript checks. Fix linting error

--- app/api/v1/webhooks/stripe/route.ts
+++ app/api/v1/webhooks/stripe/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request) {
+export async function GET() {
   // Do whatever you want
   return NextResponse.json(
     { message: '/api/v1/webhooks/stripe' },
